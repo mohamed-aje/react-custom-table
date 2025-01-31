f42e2491ffc39a62dbcdeb8dbe9f777e261bca7d
Mohamed Al-Ajily <chochoaj93@gmail.com>
2025-01-31 16:21:41 +0200

    fixed dropdown styles

--- src/util/generatedStyles.js
+++ src/util/generatedStyles.js
@@ -1,5 +1,5 @@
 const generateSelectStyles = (withBorder = false) => {
-  const borderStyle = withBorder ? `1px solid #dfe3eb` : "none";
+  const borderStyle = withBorder ? `1px solid #e0e0e0` : "none";
 
   return {
     control: (provided, state) => ({
@@ -13,18 +13,18 @@ const generateSelectStyles = (withBorder = false) => {
       },
       borderColor: state.isFocused
         ? withBorder
-          ? "#dfe3eb"
+          ? "#e0e0e0"
           : "transparent"
         : provided.borderColor,
       boxShadow: state.isFocused
-        ? "0 0 0 1px #ff2a52 !important"
+        ? "0 0 0 1px #3498db !important"
         : "none !important",
       outline: "none !important",
     }),
     option: (provided, state) => ({
       ...provided,
-      color: state.isSelected ? "white" : "black",
-      backgroundColor: state.isSelected ? "gray" : "white",
+      color: state.isSelected ? "white" : "#333333",
+      backgroundColor: state.isSelected ? "#3498db" : "white",
       "&:hover": {
         backgroundColor: "#ff2a52",
       },
@@ -55,81 +55,3 @@ const generateSelectStyles = (withBorder = false) => {
     }),
   };
 };
-
-const customSelectStyles = {
-  control: (provided) => ({
-    ...provided,
-    minHeight: "40px",
-    border: "none",
-    boxShadow: "none",
-    "&:hover": {
-      border: "none",
-    },
-    overflow: "hidden",
-  }),
-  menu: (provided) => ({
-    ...provided,
-    zIndex: 9999,
-    maxHeight: "200px",
-    overflowY: "auto",
-  }),
-  multiValue: (provided) => ({
-    ...provided,
-    backgroundColor: "#f0f0f0",
-    borderRadius: "4px",
-    display: "flex",
-    alignItems: "center",
-    padding: "0 6px",
-    margin: "2px",
-    overflow: "hidden",
-    maxWidth: "100px",
-    whiteSpace: "nowrap",
-    textOverflow: "ellipsis",
-    border: "none",
-  }),
-  multiValueLabel: (provided) => ({
-    ...provided,
-    color: "#333",
-    fontSize: "12px",
-    padding: "0",
-  }),
-  multiValueRemove: (provided) => ({
-    ...provided,
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: "#ff6666",
-      color: "#fff",
-    },
-  }),
-  valueContainer: (provided) => ({
-    ...provided,
-    display: "flex",
-    flexWrap: "wrap",
-    maxHeight: "100px",
-    overflowY: "auto",
-  }),
-  placeholder: (provided) => ({
-    ...provided,
-    color: "#aaa",
-  }),
-  input: (provided) => ({
-    ...provided,
-    margin: 0,
-    padding: 0,
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    display: "flex",
-    alignItems: "center",
-    padding: "8px 12px",
-    backgroundColor: state.isSelected ? "#e0e0e0" : "#fff",
-    color: "#333",
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: "#f5f5f5",
-    },
-    border: "none",
-  }),
-};
-
-export { generateSelectStyles, customSelectStyles };
